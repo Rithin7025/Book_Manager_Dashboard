b24fb0ae3cd22ca5c7ed1cbe2a9f286c6056e564
Rithin <rithinrj2001@gmail.com>
2024-04-23 13:19:17 +0530

    Completed controller functionality for ADD, EDIT and DELETE books

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -2,10 +2,110 @@
 import Book from "../models/book.model.js";
 
 //Controller to add New book
-export const addBook = async(req,res) => {
+export const addBook = async(req,res,next) => {
     try {
-        console.log('add book')
+        
+        const {bookName , description, publishedDate, price } = req.body ;
+
+          // Check if every details are provided
+    if(!bookName || !description || !publishedDate || !price) {
+        const BookError = new Error('Category name and description are required');
+        BookError.statusCode = 400;
+       return next(BookError)
+      }
+     //check if book already exists in database
+      const existingBook = await Book.findOne({bookName});
+      if(existingBook){
+        const BookAddError = new Error('Book Already exists');
+        BookAddError.statusCode = 409;
+        return next(BookAddError)
+      }
+
+      //creating a new book
+      const newBook = new Book({
+        bookName,
+        description,
+        publishedDate,
+        price,
+      });
+
+      //saving the new Book
+      const savedBook = await newBook.save();
+
+      return res.status(201).json({success : true , message : 'new book added',book : savedBook})
     } catch (error) {
-        console.log(error)
+       next(error)
+    }
+}
+
+
+//controller to edit book
+export const editBook = async(req,res,next) => {
+    const {bookName , description, publishedDate, price } = req.body ;
+    const bookId = req.params.id
+    try {
+         //handle error where the bookid is not provided
+    if(!bookId){
+        const BookIdError = new Error('Category Id not provided');
+        BookIdError.statusCode = 400;
+       return next(BookIdError)
+      }
+
+      const existingBook = await Book.findOne({_id : bookId});
+      //handle error when book is not found
+      if(!existingBook){
+        const BookNotFoundError = new Error('Category Not Found!!');
+        BookNotFoundError.statusCode = 404 ; 
+      return  next(BookNotFoundError)
+    }
+
+     if(bookName){
+        existingBook.bookName = bookName ; 
+     }
+     if(description){
+        existingBook.description = description;
+     }
+     if(publishedDate){
+        existingBook.publishedDate = publishedDate ; 
+     }
+     if(price){
+        existingBook.price = price;
+     }
+
+     //updating the book
+     const updatedBook = await existingBook.save();
+     return res.status(200).json({success : true , message : 'new book added',book : updatedBook})
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+//controller to delete book
+export const deleteBook = async(req,res,next) => {
+    const bookId = req.params.id
+
+    try {
+        //return error if no bookId provided
+        if(!bookId){
+            const BookIdError = new Error('Category Id not provided');
+            BookIdError.statusCode = 400;
+           return next(BookIdError)
+          }
+
+          const existingBook = await Book.findById(bookId);
+          
+          //handle error where the book is not found
+          if(!existingBook){
+            const BookError = new Error('No Book found');
+            BookError.statusCode = 404;
+            return next(BookError)
+          }
+          //delete Book from collection
+          const deletedBook = await Book.findByIdAndDelete(bookId);
+          res.status(200).json({ success: true, message: 'Book deleted successfully' })
+
+    } catch (error) {
+        next(error)
     }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
-
+import errorHandler from './middleware/errorhandler.js'
 import connectDB from './database/db.js'
 import adminRoutes from '../backend/routes/admin.route.js'
 
@@ -16,13 +16,15 @@ app.use(express.json())
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser())
 
-app.use('/api/admin',adminRoutes);
-
-
 app.get('/test',(req,res)=>{
     res.json({message : "test run"})
 })
+app.use('/api/admin',adminRoutes);
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`server started at port : ${port}`)
 })
+
+//error middleware
+app.use(errorHandler)
